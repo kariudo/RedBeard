e1602e461635f0e50225a48d7a7db3dd250fd61d
Hunter Horsman <kariudo@gmail.com>
2013-05-22 22:11:41 -0400

    Skeleton now has bones

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedBeard::Application.config.secret_token = 'dff241b1eb89908af68a8e797c95f73fc634c1a242a8a0db7079e2fd775a9b209e78878a1a432f9b17215e74b534a0fe30ebc07f39cee4ba93b603584f556408'
